b59d523d95a0e6e39e5be8762fa08735cf050d4c
rodrigotc94 <rodrigocarvalho94@gmail.com>
2020-05-16 20:08:37 -0300

    foi criado os testes unitario para o Category

---
+++ tests/Unit/CategoryTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Category;
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use PHPUnit\Framework\TestCase;
+
+class CategoryTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testFillableAttribute()
+    {
+        $fillable = ['name', 'description', 'is_active'];
+        $category = new Category();
+        $this->assertEquals($fillable, $category->getFillable());
+    }
+
+    public function testCastAttribute()
+    {
+        $cast = ['id' => 'string'];
+        $category = new Category();
+        $this->assertEquals($cast, $category->getCasts());
+    }
+
+    public function testIncrementingAttribute()
+    {
+        $category = new Category();
+        $this->assertFalse($category->incrementing);
+    }
+
+    public function testDatesAttribute()
+    {
+        $dates = ['deleted_at', 'created_at', 'updated_at'];
+        $category = new Category();
+        foreach ($dates as $date) {
+            $this->assertContains($date, $category->getDates());
+        }
+        $this->assertCount(count($dates), $category->getDates());
+    }
+
+    public function testIfUseTraits()
+    {
+        $traits = [
+            SoftDeletes::class, Uuid::class
+        ];
+        $categoryTrait = array_keys(class_uses(Category::class));
+        $this->assertEquals($traits, $categoryTrait);
+        //print_r(class_uses(Category::class));
+    }
+}
